0f43c4263a4aa4c2e7887b9319cd72c06b4d2da4
Archakov <abreak06@gmail.com>
2016-05-31 23:36:20 +0300

    add type

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ window.onload = function() {
 	chrome.extension.sendRequest({id: document.body.outerHTML.split('<a href="/albums')[1].split('"')[0]});
 
 	var script = document.createElement('script');
+	script.type = 'text/javascript';
 	script.src = 'https://raw.githubusercontent.com/Archakov06/VKRemotePlayer-Plugin/master/init.js';
 	document.head.appendChild(script);
 
